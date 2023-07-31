12841e2f81bf3f566523808c192418f4b97fbfc9
Zixi Liu <lzx1126@g.ucla.edu>
2023-07-30 21:49:03 -0700

    Update Medium.sql

--- 4-Databases/Medium.sql
+++ 4-Databases/Medium.sql
@@ -16,3 +16,11 @@ inner join employee as m
 on e.managerId=m.id
 group by e.managerId 
 having count(e.id)>=5
+
+/* 1934. Confirmation Rate */
+
+select s.user_id,
+       sum(case when action = 'confirmed' then 1 else 0 end)/count(*) as confirmation_rate
+from Signups s
+left join Confirmations c on c.user_id = s.user_id
+group by s.user_id
