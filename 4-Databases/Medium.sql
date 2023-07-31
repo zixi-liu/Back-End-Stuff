1eb086611e86a7cf5b76738110500f6b388d4512
Zixi Liu <lzx1126@g.ucla.edu>
2023-07-30 22:01:19 -0700

    Update Medium.sql

--- 4-Databases/Medium.sql
+++ 4-Databases/Medium.sql
@@ -20,7 +20,7 @@ having count(e.id)>=5
 /* 1934. Confirmation Rate */
 
 select s.user_id,
-       sum(case when action = 'confirmed' then 1 else 0 end)/count(*) as confirmation_rate
+       round(avg(if(c.action="confirmed",1,0)),2)  as confirmation_rate
 from Signups s
-left join Confirmations c on c.user_id = s.user_id
+left join Confirmations c on c.user_id = s.user_id 
 group by s.user_id
