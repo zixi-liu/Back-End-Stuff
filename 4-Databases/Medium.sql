fa412a5783addb0f0de63785c1556e87dd059456
Zixi Liu <lzx1126@g.ucla.edu>
2023-07-30 22:09:10 -0700

    Update Medium.sql

--- 4-Databases/Medium.sql
+++ 4-Databases/Medium.sql
@@ -24,3 +24,14 @@ select s.user_id,
 from Signups s
 left join Confirmations c on c.user_id = s.user_id 
 group by s.user_id
+
+/* 1193. Monthly Transactions I */
+ 
+select DATE_FORMAT(trans_date, '%Y-%m') as month,
+       country,
+       count(*) as trans_count,
+       sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+       sum(amount) as trans_total_amount,
+       sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by 1, 2
